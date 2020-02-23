22f03c68e332af7c5287680136a7ead64d4fb679
Peter McGarrigle <mcgarrigle@github.com>
2020-02-23 16:31:02 +0000

    ca created from same dn.conf as san certs

--- make-ca.sh
+++ make-ca.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# 1. Country Name (2 letter code) [XX]:GB
-# 2. State or Province Name (full name) []:
-# 3. Locality Name (eg, city) [Default City]:London
-# 4. Organization Name (eg, company) [Default Company Ltd]:Company
-# 5. Organizational Unit Name (eg, section) []:
-# 6. Common Name (eg, your name or your server's hostname) []:ca.local
-# 7. Email Address []:someone@example.com
-
-answers() {
-  echo GB
-  echo "."
-  echo London
-  echo Company
-  echo "."
-  echo ca.example.com
-  echo hostmaster@example.com
+config() {
+cat <<EOF
+[req]
+distinguished_name = dn
+default_bits = 2048
+default_md = sha256
+prompt = no
+[dn]
+$(cat dn.conf)
+CN = ROOTCA
+EOF
 }
 
-# generate key with this
-#
+config > /tmp/ca.cnf
+
 mkdir -p private
 openssl genrsa -out private/cakey.key 2048
 
-rm cacert.pem
-
-answers | openssl req -x509 -new -nodes -key private/cakey.key -sha256 -days 3650 -out cacert.pem
+rm -f cacert.pem
+openssl req -x509 -new -nodes -config /tmp/ca.cnf -key private/cakey.key -sha256 -days 3650 -out cacert.pem
 
 openssl x509 -text -noout -in cacert.pem
